df3fa4863e57e5549600b161b2fd5310b6abb803
Jermin Bazazian <jerminb@yahoo.com>
2016-01-25 17:05:58 +0800

    Initial commit

---
+++ app/src/main/java/com/hmm/main/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.hmm.main;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hmm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
