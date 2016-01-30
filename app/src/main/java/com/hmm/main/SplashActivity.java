fec01846be508499ec605c1c2ba2629489f3c83e
Jermin Bazazian <jerminb@yahoo.com>
2016-01-31 01:04:42 +0800

    FeedView

--- app/src/main/java/com/hmm/main/SplashActivity.java
+++ app/src/main/java/com/hmm/main/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, FeedActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hmm/main/feed_fragments/RecyclerViewFragment.java
@@ -0,0 +1,61 @@
+package com.hmm.main.feed_fragments;
+
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
+import com.github.florent37.materialviewpager.adapter.RecyclerViewMaterialAdapter;
+import com.hmm.R;
+import com.hmm.main.feed_adapters.TestRecyclerViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by code1 on 31/01/16.
+ */
+public class RecyclerViewFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    private static final int ITEM_COUNT = 100;
+
+    private List<Object> mContentItems = new ArrayList<>();
+
+    public static RecyclerViewFragment newInstance() {
+        return new RecyclerViewFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recyclerview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new RecyclerViewMaterialAdapter(new TestRecyclerViewAdapter(mContentItems));
+        mRecyclerView.setAdapter(mAdapter);
+
+        {
+            for (int i = 0; i < ITEM_COUNT; ++i)
+                mContentItems.add(new Object());
+            mAdapter.notifyDataSetChanged();
+        }
+
+        MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView, null);
+    }
+}
